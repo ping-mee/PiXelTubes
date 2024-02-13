dccb64666abf7523a90068bed2a11e57412c7d84
Ebbe Baß <ebbe.bass>
2024-02-13 18:44:20 +0100

    make ip string
    Signed-off-by: Ebbe Baß <ebbe.bass>

--- server/exampleReceiver.py
+++ server/exampleReceiver.py
@@ -6,7 +6,7 @@ import os
 def get_eth0_ip():
     try:
         # Get the IP address of the eth0 interface
-        eth0_ip = os.system("ip -4 -o addr show eth0 | awk '{print $4}' | cut -d '/' -f 1 ")
+        eth0_ip = str(os.system("ip -4 -o addr show eth0 | awk '{print $4}' | cut -d '/' -f 1 "))
         return eth0_ip
     except (KeyError, IndexError, OSError) as e:
         print(f"Error getting eth0 IP: {e}")
