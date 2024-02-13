dd20c48bd2eec43953f49142acf971101f907f11
Ebbe Baß <ebbe.bass>
2024-02-13 01:02:23 +0100

    fixed callback
    Signed-off-by: Ebbe Baß <ebbe.bass>

--- server/app.py
+++ server/app.py
@@ -121,13 +121,13 @@ def mqtt_publisher(universe):
         print(str(get_eth0_ip()))
         receiver.start()
 
-        @receiver.listen_on('universe', universe=universe)
-        @receiver.register_listener('universe', universe=universe, callback=callback)
         def callback(packet):  # packet type: sacn.DataPacket
             print(packet.dmxData)
             global dmx_values
             dmx_values = packet.dmxData
         receiver.join_multicast(universe)
+        receiver.listen_on('universe', universe=universe)
+        receiver.register_listener('universe', universe=universe, callback=callback)
 
         while True:
             if dmx_values is not None:
