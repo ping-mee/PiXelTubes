64dc46c96801d2ad3f06261139cde2d0928b3f8a
Ebbe Baß <ebbe.bass>
2024-02-13 21:14:22 +0100

    removed sysip
    Signed-off-by: Ebbe Baß <ebbe.bass>

--- server/exampleReceiver.py
+++ server/exampleReceiver.py
@@ -25,7 +25,7 @@ artnetUniverse = 0
 ### Art-Net Setup ###
 # Sets debug in Art-Net module.
 # Creates Artnet socket on the selected IP and Port
-artNet = Artnet.Artnet(BINDIP = artnetBindIp, SYSIP=artnetBindIp, DEBUG = False, SHORTNAME = "PiXelTubeMaster", LONGNAME = "PiXelTubeMaster", PORT = 6454)
+artNet = Artnet.Artnet(BINDIP = artnetBindIp, DEBUG = False, SHORTNAME = "PiXelTubeMaster", LONGNAME = "PiXelTubeMaster", PORT = 6454)
 
 tuple_ip = (str(get_eth0_ip()), 6454)
 while True:
