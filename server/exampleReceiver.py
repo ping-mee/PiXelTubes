a9c2e033334f08299f0eae591668bdbe9745c742
Ebbe Baß <ebbe.bass>
2024-02-15 12:31:37 +0100

    test
    Signed-off-by: Ebbe Baß <ebbe.bass>

--- server/exampleReceiver.py
+++ server/exampleReceiver.py
@@ -5,11 +5,19 @@ import os
 
 artnet = Artnet.Artnet()
 
+def get_eth0_ip():
+    try:
+        # Get the IP address of the eth0 interface
+        eth0_ip = str(os.system("ip -4 -o addr show eth0 | awk '{print $4}' | cut -d '/' -f 1 "))
+        return eth0_ip
+    except (KeyError, IndexError, OSError) as e:
+        print(f"Error getting eth0 IP: {e}")
+        exit
 
 while True:
     try:
         
-        artNetPacket = artnet.readPacket()
+        artNetPacket = artnet.readPacket(BINDIP=get_eth0_ip)
         if artNetPacket is not None and artNetPacket.data is not None:
             if artNetPacket.universe == 0:
                 print("Universe was the specified universe: "+str(artNetPacket.universe))
