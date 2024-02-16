bc6f1b4ab12d4223e01d6d815dc208f892a2e65b
Ebbe Baß <ebbe.bass>
2024-02-16 01:48:32 +0100

    looking into ver
    Signed-off-by: Ebbe Baß <ebbe.bass>

--- server/exampleReceiver.py
+++ server/exampleReceiver.py
@@ -28,7 +28,7 @@ while True:
     try:
         # Gets whatever the last Art-Net packet we received is
         artNetPacket = artNet.readPacket()
-        print(artNetPacket.sequence)
+        print(artNetPacket.ver)
         
     except KeyboardInterrupt:
         break
