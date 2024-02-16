a4ec98b6f0250348f13e299f3093168c91a2bd3f
Ebbe Baß <ebbe.bass>
2024-02-16 01:39:38 +0100

    checking what is in length
    Signed-off-by: Ebbe Baß <ebbe.bass>

--- server/exampleReceiver.py
+++ server/exampleReceiver.py
@@ -28,7 +28,7 @@ while True:
     try:
         # Gets whatever the last Art-Net packet we received is
         artNetPacket = artNet.readPacket()
-        print(artNetPacket.data)
+        print(artNetPacket.length)
         
     except KeyboardInterrupt:
         break
