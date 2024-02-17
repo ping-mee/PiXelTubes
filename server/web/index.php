9088fc82484824e4c7e124fdfddb43f212403b94
Ebbe Baß <ebbe.bass>
2024-02-17 01:55:52 +0100

    added configure button
    Signed-off-by: Ebbe Baß <ebbe.bass>

--- server/web/index.php
+++ server/web/index.php
@@ -46,7 +46,7 @@
                             <th>'.$row["dmx_address"].'</th>
                             <th>
                                 <form action="./settings.php?id='.$row["mac_address"].'">
-                                    <button type="submit" class="btn btn-secondary">Configure</button>
+                                <button type="button" class="btn btn-secondary" data-toggle="modal" data-target="#configureModal" data-id="'.$row["mac_address"].'" data-universe="'.$row["universe"].'" data-dmx-address="'.$row["dmx_address"].'">Configure</button>
                                 </form>                            
                             </th>
                         </tr>
