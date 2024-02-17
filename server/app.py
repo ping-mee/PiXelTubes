2d0418ee0e22edeb35f33cd00df02a29d32da68f
Ebbe Baß <ebbe.bass>
2024-02-17 03:00:09 +0100

    debug
    Signed-off-by: Ebbe Baß <ebbe.bass>

--- server/app.py
+++ server/app.py
@@ -127,6 +127,7 @@ def start_mqtt_publishers():
                 cur.execute("SELECT mac_address, universe, dmx_address FROM tubes")
                 result = cur.fetchall()
                 for row in result:
+                    print(row)
                     dmx_address = int(row[2])
                     if artNetPacket.universe == int(row[2]):
                         #Define RGB values per pixel
