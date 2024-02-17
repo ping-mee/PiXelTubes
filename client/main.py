6e1928798366fca50b8c1e09cfb20fc0240c9be0
Ebbe Baß <ebbe.bass>
2024-02-17 03:47:51 +0100

    added loop start
    Signed-off-by: Ebbe Baß <ebbe.bass>

--- client/main.py
+++ client/main.py
@@ -104,4 +104,4 @@ if __name__ == "__main__":
         mqttc.subscribe("tube-"+str(wlan_mac_address)+"/p5", 0)
         mqttc.subscribe("tube-"+str(wlan_mac_address)+"/p6", 0)
 
-        mqttc.loop_forever()
+        mqttc.loop_start()
