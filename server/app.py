bb9f5c8f2ab4695d9ef52388b0805aed47a07ecf
Ebbe Baß <ebbe.bass>
2024-02-18 15:46:47 +0100

    made tube_index tuple when received
    Signed-off-by: Ebbe Baß <ebbe.bass>

--- server/app.py
+++ server/app.py
@@ -116,12 +116,11 @@ def mqtt_publisher(ti_receiver):
     artNet = Artnet.Artnet(BINDIP = artnetBindIp, DEBUG = True, SHORTNAME = "PiXelTubeMaster", LONGNAME = "PiXelTubeMaster", PORT = 6454)
     while True:
         try:
-            tube_index = ti_receiver.recv()
+            tube_index = tuple(ti_receiver.recv())
         except:
             tube_index = tube_index_old
 
         tube_index_old = None
-        print(tube_index)
         try:
             # Gets whatever the last Art-Net packet we received is
             artNetPacket = artNet.readPacket()
